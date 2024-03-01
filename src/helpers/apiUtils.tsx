e9183d04472ddc5cb57c727eb4d57415fcdb98ca
unknown <lufealsu@hotmail.com>
2024-02-29 19:43:25 -0500

    cors again

--- src/helpers/apiUtils.tsx
+++ src/helpers/apiUtils.tsx
@@ -7,7 +7,7 @@ type ApiResponse = ApiResponseNeeded & {
   [key: string]: unknown;
 };
 
-const endPoint = "https://api.short.io/links";
+const endPoint = '/getlink'; /* "https://api.short.io/links" */;
 const apikey = 'sk_mPLmRVYAfBxhCfqr' || import.meta.env.VITE_REBRANDLY_API_KEY;
 const headers = { authorization: apikey, "content-type": "application/json" };
 
@@ -18,7 +18,7 @@ export const getShortenedUrl = async (originalURL: string): Promise<ApiResponse>
     domain: "d9kg.short.gy",
   });
 
-  const response = await fetch(endPoint, { headers, body, method: "POST", mode: 'no-cors' });
+  const response = await fetch(endPoint, { headers, body, method: "POST" });
   if (!response.ok) {
     alert("A website error has occurred. Sorry for the temporary inconvenience.");
 
